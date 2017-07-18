d14476d4c9b8f04551b7d510ad4c73283bcee88a
Allen Lyons <allen.lyons@gmail.com>
2017-07-18 18:55:38 -0400

    updated help text

--- lambda/hello.js
+++ lambda/hello.js
@@ -14,7 +14,9 @@ exports.handler = (event, context, callback) => {
 
     console.dir(event);
 
-    response.dialogAction.message.content = 'Hello! I am the Lex example for the bot whisperer project. You can ask me questions about the project such as: What is your purpose? Or, what can you do? Press and hold down the microphone button to speak to me.';
+    response.dialogAction.message.content = 'Hello! I am the Lex example for the bot whisperer project. You can ask me questions about the project such as: What can you do? Or. What is the debug panel? Press and hold down the microphone button to speak to me.';
+
+
 
     callback(null, response);
 };
